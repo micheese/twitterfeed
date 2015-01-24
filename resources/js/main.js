9cc54bbed0b4c7267fec9f47d7d68ec961540d34
Michael Chemani <michael_chemani@hotmail.com>
2015-01-24 14:08:13 -0500

    v2

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,26 @@
 /**
  * Created by michael on 2015-01-17.
  */
-$(function () {
+/*$(function () {
     $('.tlt').textillate({ loop: true });
-})
+})*/
+
+$(document).ready(function(){
+    var attemp_count = 0;
+
+    var auto_refresh = setInterval(function()
+    {
+        $.ajax({
+            url: 'twuserlookup.php'
+        }).done(function(data)
+        {
+            for (index = 0; index < data.length; ++index) {
+                $('#mydiv').append(data[index]);
+            }
+        });
+        if(attemp_count > 775)
+        {
+            clearInterval(auto_refresh);
+        }
+    }, 6000);
+});
